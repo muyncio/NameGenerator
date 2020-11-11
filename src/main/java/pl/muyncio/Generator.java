118a814ca1f39cdee364e72636abcf56b9adccb0
muyncio <muynek@gmail.com>
2020-11-11 14:40:40 +0100

    Adding hundred names...

--- src/main/java/pl/muyncio/Generator.java
+++ src/main/java/pl/muyncio/Generator.java
@@ -1,18 +1,18 @@
 package pl.muyncio;
 
 public class Generator {
-    String [] names = {"", "", "", "", "", "", "", "", "", "",
-                        "", "", "", "", "", "", "", "", "", "",
-                        "", "", "", "", "", "", "", "", "", "",
-                        "", "", "", "", "", "", "", "", "", "",
-                        "", "", "", "", "", "", "", "", "", "",
-                        "", "", "", "", "", "", "", "", "", "",
-                        "", "", "", "", "", "", "", "", "", "",
-                        "", "", "", "", "", "", "", "", "", "",
-                        "", "", "", "", "", "", "", "", "", "",
-                        "", "", "", "", "", "", "", "", "", "",};
+    String[] names = {"Ada", "Adela", "Adelajda", "Alina", "Alicja", "Agnieszka", "Amanda", "Andżelika", "Aneta", "Apolonia",
+                        "Barbara", "Bianka", "Celestyna", "Czesława", "Dagna", "Daria", "Dominika", "Eliza", "Ewa", "Fiona",
+                        "Faustyna", "Franciszka", "Gabriela", "Grzymisława", "Halina", "Helga", "Ilona", "Izabela", "Jagoda", "Jolanta",
+                        "Kaja", "Karolina", "Kinga", "Magdalena", "Aleksandra", "Marzena", "Olga", "Pamela", "Rita", "Róża",
+                        "Sara", "Samanta", "Tamara", "Teresa", "Ula", "Wioleta", "Violetta", "Władysława", "Zdzisława", "Żaneta",
+                        "Adam", "Alan", "Andrzej", "Baltazar", "Borys", "Bruno", "Cezar", "Cyprian", "Czesław", "Dariusz",
+                        "Denis", "Dominik", "Donald", "Dymitr", "Edward", "Erwin", "Emil", "Eryk", "Fabian", "Filip",
+                        "Gabriel", "Gracjan", "Gustaw", "Hasan", "Herbert", "Hugo", "Igor", "Izydor", "Jacek", "Jan",
+                        "Janusz", "Jonasz", "Kacper", "Lambert", "Leon", "Łukasz", "Maciej", "Marcin", "Mariusz", "Nikodem",
+                        "Oleg", "Pankracy", "Rafał", "Sławek", "Szymon", "Tadeusz", "Tomasz", "Wacław", "Grzegorz", "Zenon"};
 
-    String [] surnames= {"", "", "", "", "", "", "", "", "", "",
+    String[] surnames= {"", "", "", "", "", "", "", "", "", "",
                         "", "", "", "", "", "", "", "", "", "",
                         "", "", "", "", "", "", "", "", "", "",
                         "", "", "", "", "", "", "", "", "", "",
